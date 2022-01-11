eabf8134d1c057cf34705214442af095ff57e079
S. N. Valadez <88351213+SNValadez@users.noreply.github.com>
2022-01-10 17:56:51 -0800

    tests and reducers

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -3,7 +3,13 @@ import { useReducer } from 'react';
 import {
     UPDATE_PRODUCTS,
     UPDATE_CATEGORIES,
-    UPDATE_CURRENT_CATEGORY
+    UPDATE_CURRENT_CATEGORY,
+    ADD_TO_CART,
+  ADD_MULTIPLE_TO_CART,
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  CLEAR_CART,
+  TOGGLE_CART
   } from "./actions";
   
   export const reducer = (state, action) => {
